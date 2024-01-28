354e2972f807c0f431b223c6cac7214b2936bf73
QXIoa <superteddy888@gmail.com>
2024-01-27 19:56:32 -0500

    Removed time library from alarm.py, fixed indents

--- alarm.py
+++ alarm.py
@@ -1,13 +1,15 @@
 from datetime import datetime
 from waiter import Wait as wait
-import winsound, random, time
+import winsound, random
 
 alarm_date=input('Enter the date on which you want to set the alarm: ').strip()
 alarm_time=''.join(input("Enter the time of alarm to be set in HH:MM,AM/PM format: ").split())
 music_or_beep = input("Enter m for a music or b for beep sound: ")
+
 if music_or_beep=='b':
     dur=int(input("duration in seconds: "))*1000 #winsound takes in milliseconds
     freq = int(input("frequency of the noise: ")) #optimal- 500
+
 alarm_hour=alarm_time[0:2]
 alarm_minute=alarm_time[3:5]
 alarm_period=alarm_time[6:8].upper()
